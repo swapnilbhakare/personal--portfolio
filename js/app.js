1f2aa6c26aa9ec466566557390ce8c59b04ff93a
swapnil bhakare <67636219+swapnilbhakare@users.noreply.github.com>
2021-10-17 12:44:38 +0530

    navbar done and home done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
+// preloader script
 const preloader = document.querySelector(".preloader");
 window.addEventListener('load',()=>{
     setTimeout(()=>{
         preloader.classList.add('hide-preloader')
     },2000)
 })
+
+// slide image animation
+// slides fade animation
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+setInterval(function () {
+  plusSlides(1);
+}, 3 * 1000);
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("img");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "flex";
+}
